9a9b7a76f78b3fd68d7098a84691f14bf049be3b
WithLithum <WithLithum@foxmail.com>
2022-03-08 18:47:21 +0800

    :tada: Initial commit

---
+++ WithLithum.Core.Response/DispatchUtil.cs
@@ -0,0 +1,51 @@
+﻿namespace WithLithum.Core.Response;
+
+using Rage;
+using Rage.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class DispatchUtil
+{
+    /// <summary>Displays an notification showing the speaker speaking the text.</summary>
+    /// <param name="speaker">The speaker.</param>
+    /// <param name="text">The text.</param>
+    /// <returns>The handle of the notification.</returns>
+    /// <remarks>
+    /// <c>{player}</c> in the text will be replaced to the player's character name
+    /// (e.g. Jenny Sandusky).
+    /// </remarks>
+    public static uint DisplayRadioQuote(string speaker, string text)
+    {
+        return Game.DisplayNotification($"~b~{speaker}~w~: {text.Replace("{player}", Functions.GetPersonaForPed(Game.LocalPlayer.Character).FullName)}");
+    }
+
+    /// <summary>Displays the radio quote using position.</summary>
+    /// <param name="speaker">The speaker.</param>
+    /// <param name="text">The text.</param>
+    /// <param name="position">The position.</param>
+    /// <param name="street">if set to <c>true</c>, displays the street name.</param>
+    /// <returns>
+    ///   The handle of the notification.
+    /// </returns>
+    /// <remarks>
+    /// <c>{position}</c> in the text will be replaced to the position display name, and <c>{player}</c> in the text will be replaced to the player's character name
+    /// (e.g. Jenny Sandusky).
+    /// </remarks>
+    public static uint DisplayRadioQuoteUsingPosition(string speaker, string text, Vector3 position, bool street = false)
+    {
+        string zoneName = NativeFunction.Natives.GET_NAME_OF_ZONE<string>(position.X, position.Y, position.Z);
+        var streetName = World.GetStreetName(World.GetStreetHash(position));
+
+        var result = Game.GetLocalizedString(zoneName);
+        if (street)
+        {
+            result = $"{streetName}, {result}";
+        }
+
+        return DisplayRadioQuote(speaker, text.Replace("{position}", result));
+    }
+}
---
+++ WithLithum.Core.Response/SpawnerUtil.cs
@@ -0,0 +1,37 @@
+﻿namespace WithLithum.Core.Response;
+
+using Rage;
+using Rage.Native;
+
+public static class SpawnerUtil
+{
+    /// <summary>Gets a position around the player (usually on motorized roads) which is relatively safer for <see cref="Vehicle"/>s to cruise.</summary>
+    /// <param name="minimumDistance">The minimum distance.</param>
+    /// <param name="maximumDistance">The maximum distance.</param>
+    /// <returns>
+    ///   <para>A <see cref="Vector3" /> indicating the generated spawn point.</para>
+    /// </returns>
+    public static Vector3 GetRandomPositionOnStreet(float minimumDistance, float maximumDistance)
+    {
+        return World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(minimumDistance, maximumDistance));
+    }
+
+    /// <summary>Gets a position around the player (usually on sidewalk) which is relatively safer for <see cref="Ped" />s to walk.</summary>
+    /// <param name="minimumDistance">The minimum distance.</param>
+    /// <param name="maximumDistance">The maximum distance.</param>
+    /// <returns>If success, the generated position; otherwise, returns <see cref="Vector3.Zero" />.<br /></returns>
+    public static Vector3 GetRandomPositionOnSidewalk(float minimumDistance, float maximumDistance)
+    {
+        var vector = GetRandomPositionOnStreet(minimumDistance, maximumDistance);
+        var result = Vector3.Zero;
+
+        if (NativeFunction.Natives.GET_SAFE_COORD_FOR_PED<bool>(vector.X, vector.Y, vector.Z, false, ref result, 28))
+        {
+            return result;
+        }
+        else
+        {
+            return Vector3.Zero;
+        }
+    }
+}
